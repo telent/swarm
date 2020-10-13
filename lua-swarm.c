a6c52b085d6bf5d14d43bd0bb1f198e593237402
Daniel Barlow <dan@telent.net>
2020-10-13 23:14:12 +0100

    delete example code that we copy-pasted

--- lua-swarm.c
+++ lua-swarm.c
@@ -150,36 +150,6 @@ main(int argc, char *argv[])
         exit(1);
     }
 
-    /*
-     * Ok, now here we go: We pass data to the lua script on the stack.
-     * That is, we first have to prepare Lua's virtual stack the way we
-     * want the script to receive it, then ask Lua to run it.
-     */
-    lua_newtable(L);    /* We will pass a table */
-
-    /*
-     * To put values into the table, we first push the index, then the
-     * value, and then call lua_rawset() with the index of the table in the
-     * stack. Let's see why it's -3: In Lua, the value -1 always refers to
-     * the top of the stack. When you create the table with lua_newtable(),
-     * the table gets pushed into the top of the stack. When you push the
-     * index and then the cell value, the stack looks like:
-     *
-     * <- [stack bottom] -- table, index, value [top]
-     *
-     * So the -1 will refer to the cell value, thus -3 is used to refer to
-     * the table itself. Note that lua_rawset() pops the two last elements
-     * of the stack, so that after it has been called, the table is at the
-     * top of the stack.
-     */
-    for (i = 1; i <= 5; i++) {
-        lua_pushnumber(L, i);   /* Push the table index */
-        lua_pushnumber(L, i*2); /* Push the cell value */
-        lua_rawset(L, -3);      /* Stores the pair in the table */
-    }
-
-    /* By what name is the script going to reference our table? */
-    lua_setglobal(L, "foo");
     lua_register(L, "inotify_init", l_inotify_init);
     lua_register(L, "inotify_add_watch", l_inotify_add_watch);
     lua_register(L, "sigchld_fd", l_sigchld_fd);
