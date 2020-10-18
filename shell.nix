f58f7e54e71732892d2f22b08f7debbd53c75c50
Daniel Barlow <dan@telent.net>
2020-10-18 14:46:56 +0100

    fix PROJECT_ROOT settting properly
    https://discourse.nixos.org/t/how-to-refer-to-current-directory-in-shell-nix/9526/4?u=telent

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 with import <nixpkgs> {};
 (callPackage ./default.nix {}).overrideAttrs(o: rec {
   SWARM_BASE_PATH = "/tmp/services";
-  PROJECT_ROOT = builtins.getEnv "PWD";
+  PROJECT_ROOT = builtins.toString ./.;
   LUA_PATH = "${PROJECT_ROOT}/lib/?.lua";
   nativeBuildInputs = [ pkgs.foreman ] ;
 })
