73ed6e8807d2bc2781bb40206708e97fb6ed7a95
Daniel Barlow <dan@telent.net>
2020-10-31 16:04:56 +0000

    pass the script arguments to watcher as config
    this makes e.g. ip_state_json a bit simpler as it can get the path
    of ip(8)

--- scripts/ethernet.lua
+++ scripts/ethernet.lua
@@ -4,16 +4,17 @@ local swarm = require("swarm")
 local json = require("json")
 local inspect = require("inspect")
 
-function ip_state_json(w, ip, command)
-   local o = w:spawn(ip,
+function ip_state_json(w, command)
+   print(inspect(w.config))
+   local o = w:spawn(w.config.paths.ip,
 		     {"ip", "-j", table.unpack(command)},
 		     { wait = true, capture = true })
    return json.decode(o)[1]
 end
 
-function get_state(w, ip, ifname)
-   local linkstate = ip_state_json(w, ip, {"link", "show", ifname})
-   local addrstate = ip_state_json(w, ip, {"address", "show", ifname})
+function get_state(w, ifname)
+   local linkstate = ip_state_json(w, {"link", "show", ifname})
+   local addrstate = ip_state_json(w, {"address", "show", ifname})
    if linkstate and addrstate then
       local healthy = linkstate.operstate == "UP" and
 	 f.find(function(x) return (x.scope == "global") end,
@@ -39,13 +40,13 @@ function run(arguments)
    local iface = arguments.iface
    print("service " .. name , "if " .. iface)
 
-   local w = swarm.watcher()
+   local w = swarm.watcher(arguments)
 
    -- XXX need arguments to tell us if we should be configring using dhcp6
    -- w:subscribe("dhcp6c", {"address", "routes", "netmask"})
 
    while true do
-      swarm.write_state(name, get_state(w, arguments.paths.ip, iface))
+      swarm.write_state(name, get_state(w, iface))
       for event in w:events() do
 	 -- if event.changed and event:changed("dhcp6c", {"address", "netmask"}) then
 	 --    local dhcp6c = event.values.dhcp6c
