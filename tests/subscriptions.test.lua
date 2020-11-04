70e47bea5a99c0f9be63476ab1fdb2b15f1b4a38
Daniel Barlow <dan@telent.net>
2020-11-04 14:15:22 +0000

    subscriptions.test - refactor to extract common function

--- tests/subscriptions.test.lua
+++ tests/subscriptions.test.lua
@@ -1,6 +1,17 @@
 local swarm = require("swarm")
 local inspect = require("inspect")
 
+function find_event(fn)
+   local received = false
+   for event in w:events(0.1) do
+      if event.changes and fn(event) then
+	 received = event
+      end
+   end
+   return received
+end
+
+-----------------------
 -- given a service exists with some values
 
 swarm.write_state("a_service", {key1 = "val1", key2 = "val2"  })
@@ -14,32 +25,27 @@ w:subscribe("a_service", {"key1"})
 swarm.write_state("a_service", {key1 = "octopus", key2 = "val2"  })
 
 -- I am notified and I can see the new value
-local received = false
-for event in w:events(0.1) do
-   if event.changes and
-      event.changes["a_service"]["key1"] and
-      w.values.a_service.key1 == "octopus" then
-      received = true
-   end
-end
-assert(received, "missing event")
+assert(find_event(function (event)
+	     return event.changes["a_service"]["key1"] and
+		w.values.a_service.key1 == "octopus"
+		 end),
+       "missing event")
 
--- when I change only value(s) I am not subscribed to
+-----------------------
+-- given I subscribe to a service that exists with some values
 
-swarm.write_state("a_service", {key1 = "val1", key2 = "val2"  }) -- reset
+swarm.write_state("a_service", {key1 = "val1", key2 = "val2"  })
 w = swarm.watcher({})
 w:subscribe("a_service", {"key1"})
-swarm.write_state("a_service", {key1 = "val1", key2 = "val2"  })
+
+-- when I change only value(s) I am not subscribed to
+swarm.write_state("a_service", {key1 = "val1", key2 = "val3"  })
 
 -- I am not notified
-local received = false
-for event in w:events(0.1) do
-   if event.changed then
-      received = event
-   end
-end
-assert(not received, "received event unexpectedly: " .. inspect(received))
+e = find_event(function (e) return e end)
+assert(not e, "received event unexpectedly: " .. inspect(e))
 
+-----------------------
 -- given a service exists
 swarm.write_state("a_service", {key1 = "val1", key2 = "val2"  })
 
@@ -51,16 +57,13 @@ w:subscribe("a_service", {"key10"})
 swarm.write_state("a_service", {key1 = "val1", key2 = "val2", key10 = "heart"})
 
 -- I am notified
-local received = false
-for event in w:events(0.1) do
-   if event.changes and
-      event.changes["a_service"]["key10"] and
-      w.values.a_service.key10 == "heart" then
-      received = true
-   end
-end
-assert(received, "missing event")
+assert(find_event(function (event)
+	     return event.changes["a_service"]["key10"] and
+		w.values.a_service.key10 == "heart"
+		 end),
+       "event not found")
 
+-----------------------
 -- given a service does *not* exist
 -- and I have subscribed to a value that is not present
 w = swarm.watcher({})
@@ -70,15 +73,11 @@ w:subscribe("service_not_started", {"key10"})
 swarm.write_state("service_not_started", {key10 = "heart"})
 
 -- I am notified
-local received = false
-for event in w:events(0.1) do
-   if event.changes and
-      event.changes["service_not_started"]["key10"] and
-      w.values.service_not_started.key10 == "heart" then
-      received = true
-   end
-end
-assert(received, "missing event")
+assert(find_event(function (event)
+	     return event.changes["service_not_started"]["key10"] and
+		w.values.service_not_started.key10 == "heart"
+		 end), "missing event")
+
 
 -- given a service exists with some values
 -- and I have subscribed to one of them
